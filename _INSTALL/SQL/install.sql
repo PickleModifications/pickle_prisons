9d9b0b118e88061d15b6690257409884648fbf54
Pickle Mods <picklemodifications@gmail.com>
2023-05-25 19:19:42 -0400

    Add support for older MySQL versions.

--- _INSTALL/SQL/install.sql
+++ _INSTALL/SQL/install.sql
@@ -2,6 +2,6 @@ CREATE TABLE IF NOT EXISTS `pickle_prisons` (
   `identifier` varchar(46) NOT NULL,
   `prison` varchar(50) DEFAULT 'default',
   `time` int(11) NOT NULL DEFAULT 0,
-  `inventory` longtext DEFAULT '[]',
+  `inventory` longtext NOT NULL,
   `sentence_date` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
